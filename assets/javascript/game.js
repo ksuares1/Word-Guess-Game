dcf58b94ad6b57e0ee346f787b10a14179393a76
Kimberly Suares <kimberlyjsuares@gmail.com>
2019-11-09 14:18:40 -0500

    welcome popup

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,11 @@
 // console.log("game.js");
 
-var email = prompt;
-function welcome() {
-    alert("Welcome to Kiehl's Holiday Wonderland! Let's make gifting easy this year.")
-}
-
 // var email = prompt;
-// for(var i=0; i< prompt.length; i++){
-//     alert("What is your email address? We will email your gift ideas at the end of the game.")
+// function welcome() {
+//     alert("Welcome to Kiehl's Holiday Wonderland! Let's make gifting easy this year.")
 // }
 
-// alert("Thank you!");
+var email = prompt;
+for(var i=0; i< prompt.length; i++){
+}
+alert("Welcome to Kiehl's Holiday Gift Guide Game!");
