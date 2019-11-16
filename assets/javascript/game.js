4deeec310a202bb5984ebb7f58ad6c12da138ce7
Kimberly Suares <kimberlyjsuares@gmail.com>
2019-11-16 10:03:09 -0500

    updates

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,11 +3,10 @@
 // This game will ask players a series of questions for every one they get correct, that gift set or product will pop-up.
 // For every answer they get incorrect it will recommend another gift set to them. 
 //A word bank is provided for players, to assist them in selecting the correct answer.
-// A button feature allows players to select a word from the word bank.
 
 // console.log("game.js");
 
-
+$(document).ready(function() {
 // Create a prompt that asks players for their email address, so all recommended products can be emailed to them.
 var intro = prompt;
 prompt("Happy Holidays! Who are you shopping for today?");
@@ -22,7 +21,7 @@ prompt("Please enter your email address, so you can receive all of your Kiehl's 
     // };
 
     // List products
-    var products=["creme de corps", "ultra facial cream", "calendula deep foaming face wash", "calendula toner", "midnight recovery oil concentrate", "ultimate hand salve", "pure vitality"];
+    var products=["Creme De Corps", "Ultra Facial Cream", "Calendula Deep Foaming Face Wash", "Calendula Toner", "Midnight Recovery Oil concentrate", "Ultimate Hand Salve", "Pure Vitality"];
     var randomWords;
     var wins = 0;
     var loses = 0;
@@ -43,11 +42,7 @@ prompt("Please enter your email address, so you can receive all of your Kiehl's 
 
     gameLayout();
     
-    document.onkeyup = function(event) {
-        var player = event.key;
-        if(player === "a") || (player === "b") 
-        alendula Deep Foaming Face Wash") 
-           (player === "Calendula Toner") || (player === "Midnight Recovery Oil Concentrate") ||
-           (player === "Ultimate Hand Salve") || (player === "Pure Vitality") 
+    //   Use this array to create buttons on the screen.
+    var letters= ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "_"];
+    for (var i = 0; i < letters.length; i++) {
     }
-           console.log(event);
